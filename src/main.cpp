54984a6c26cef220df3891445e65f82fb62180b0
aeanadgrleey <orel.andreas@gmail.com>
2020-12-23 20:55:39 +0300

    Deleted some extra funcs, in process of rewriting

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,44 @@
 #include <EEPROM.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
-#include <DS3231.h>
-#include "LedControl.h"
-//standart position in schedule: hhmml; h - hours, m - minutes, l - lenth of the bell ring;
-int standartSchedule[18];//arr about work days(using at workdays)
-int currentSchedule[18];//current schedule(using at workdays and Saturdays(filled with suitable positions))
-int rawSaturdaySchedule[4];//arr about saturday's lessons(using in satMode to fill currentSchedule). No ring time
-int saturdaySchedule[18];//arr with timed saturday's rings.
-int button_time = 0;//variable about how long button was pressed
-int sortCount; //count variable for sorting
-bool setupBlu = false;//If standart schedule is set - true, else - false.
-bool satSetup = false;//If Saturday schedule is set - true, else - false.
-int i, k, j;
-int bzzCount = 0;
-unsigned long lesson_check_period = (long)1000 * 60 * 5; //Ringing waiting period lenght
-unsigned long bnb_check_period = (long) 1000 * 5; //Wait period for button and Blutooth
-unsigned long timer_one;
-unsigned long timer_two;
-bool h12, PM;
+#include <Arduino.h>
+//values for program
 
-SoftwareSerial BTserial(2, 3); // Blutooth module pins
-LedControl lc=LedControl(5,3,4,1);
-DS3231 Clock;
+//data about schedule and alarms
+int Schedule[7][20][3];//arr about work days(using at workdays). Format of every elem: [[hour, minute, lenth], ... [hour, minute, lenth],]
+int currentAlarm[3];
+
+//timers
+unsigned long lesson_check_period = (long)1000 * 60 * 5; //Ringing waiting period lenght
+unsigned long btn_check_period = (long) 1000 * 5; //Wait period for button and Blutooth
+
+//values for devices
+
+//RTC:
+RTC_DS3231 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+//display
+LedControl lc=LedControl(12,11,10,1);
+
+//button
 int button = 13;
-int LED1_PIN = 7;
+
+//LEDs
+int LED1 = 7;
+int LED2
+
+//relay
 int RingRelay = 4;
 
 void setup()
 {
-  EEPROM.get(255, setupBlu);
   Serial.begin(9600);
-  BTserial.begin(9600);
   Wire.begin();
-  //  setupBlu = EEPROM.readInt(111);
-  if (setupBlu = true) {
-    scheduleRead();
-  }
+  scheduleRead();
   timer_one = millis();
   timer_two = millis();
+  // Rewriting again
   lc.shutdown(0,false);
   lc.setIntensity(0,8);
   lc.clearDisplay(0);
@@ -46,18 +46,13 @@ void setup()
 //Main loop. Responsible for bell rings.
 void loop()
 {
+  //Whole main loop is full of errors, because new library
+  //Some functions be like afsgshth wareehtr
   //Wait for lesson_check_period
   if (millis() - timer_one >= lesson_check_period) {
     //If midnight, count for rings will be zero
-    if ((Clock.getHour(h12, PM) == 0) and (Clock.getMinute() <= 5)) {
+    if ((Clock.getHour(h12, PM) == 0) and (Clock.getMinute() <= 10)) {
       bzzCount = 0;
-      memcpy(standartSchedule, currentSchedule, 18);
-      satSetup = false;
-    }
-    //Check if Dow is saturday and schedule isn't set up for saturday
-    if (Clock.getDoW() == 6 and satSetup == false) {
-      memcpy(saturdaySchedule, currentSchedule, 18);
-      satSetup = true;
     }
     //Main part of the bell
     //Check if current hour is equal to current ring hour and if it workday. If yes, setting wait period to shorter
@@ -75,81 +70,25 @@ void loop()
       lesson_check_period = (long) 1000 * 60;
     }
   }
-  if (millis() - timer_two >= bnb_check_period) {
+  if (millis() - timer_two >= btn_check_period) {
     button_check();
-    if (BTserial.available() && setupBlu == false) {
-      bluReceive();
 
     }
+  }
+  while (millis() - timer_three >= 100) {
+
   }
   display_digits(Clock.getHour(h12, PM), Clock.getMinute(), currentSchedule[bzzCount]);
   Serial.print(Clock.getHour(h12, PM));
   Serial.println(Clock.getMinute());
 }
-//receiving Data. Because I had some problems with reciving more than 64 numbers, I have to use this method.
-void bluReceive() {
-  digitalWrite(LED1_PIN, HIGH); // Let user know that, arduino knows that his phone connected
-  scheduleReset();
-  int rawRing[4];//local array contains info about lesson's start and end
-  int countA = 0;
-  int countB = 0;
-  int countSat = 0;
-  int receivedData;
-  if (BTserial.read() == 0) {
-    unsigned long timer = millis(); // Reseting timer
-    //Receiving data about workday
-    while (countA < 18) { //
-      while (countB < 5) { //reciving data about one lesson
-        // Wait for 0.1 second
-        if (millis() - timer >= 100) {
-          //Android is sending wrong digits, which is greater by 48. That's very strange.
-          rawRing[countB] = BTserial.read() - 48;
-          countB += 1;
-          timer = millis(); //Reseting timer to wait for some time.
-        }
-      }
-      //Sending array of digits to tranform it into one big number
-      scheduleSort(rawRing);
-      countA += 1;
-    }
-    timer = millis();
-    //receiving data about saturday
-    while (countSat < 5) {
-      if (millis() - timer >= 100) {
-        receivedData = BTserial.read() - 48;
-        if (millis() - timer >= 200) {
-          saturdaySchedule[countSat] = receivedData * 10 + (BTserial.read() - 48);
-        }
-      }
-    }
-    setupBlu = true;
-    EEPROM.put(255, setupBlu);
-    //EEPROM.update(111, setupBlu);
-    scheduleWrite(standartSchedule, saturdaySchedule);
-    digitalWrite(LED1_PIN, LOW);
-  }
-}
+
+//Looks strange
 //Collecting array of time pieces into one integer
 void scheduleSort(int*ringTime) {
   standartSchedule[sortCount] = ringTime[0] * 10000 + ringTime[1] * 1000 + ringTime[2] * 100 + ringTime[3] * 10 + ringTime[4];
 }
-//Writing current schedules to EEPROM
-void scheduleWrite(int*schedule_std, int*schedule_sat) {
-  int k, i = 0;
-  while (i < 18) {
-    EEPROM.put(k, standartSchedule[i]);
-    i++;
-    k = k + 2;
-  }
-  i = 0;
-  while (i < 4) {
-    EEPROM.put(k, saturdaySchedule[i]);
-    k = k + 2;
-    i++;
-  }
-}
-//Almost the same as Writing
-
+// Probblems!!! Wrong reading
 void scheduleRead() {
   int k, i = 0;
   while (i < 18) {
@@ -157,38 +96,8 @@ void scheduleRead() {
     i++;
     k = k + 2;
   }
-  i = 0;
-  while (i< 4) {
-    EEPROM.get(k, saturdaySchedule[i]);
-    k = k + 2;
-    i++;
-  }
 }
-
-//Creating schedule for saturday
-
-void saturdayScheduleMaker() {
-  int countSat;
-  int currTime = saturdaySchedule[1];
-  scheduleReset();
-  for (int i = 0; i < saturdaySchedule[0] * 2 + 1; i++) {
-    for (int k; k < 2; k++) {
-      if (currTime % 100 + saturdaySchedule[2 + k] >= 60) {
-        currTime = (currTime / 100 + 1) * 100 + (currTime % 100 + saturdaySchedule[2 + k]) % 60;
-      }
-      else {
-        currTime = currTime + saturdaySchedule[2 + k];
-      }
-      currentSchedule[i] = currTime * 10;
-    }
-  }
-  setupBlu = true;
-}
-void scheduleReset() {
-  for (int i = 0; i < 18; i++) {
-    currentSchedule[i] = 0;
-  }
-}
+//Needs some repair
 void button_check() {
   bool first_pos, second_pos;
   unsigned long local_timer = millis();
@@ -224,8 +133,9 @@ void bzzz_mode(int lenght){
   }
   digitalWrite(RingRelay, LOW);
 }
-void display_digits(int now_hours, int now_mins, int closest_ring){
+void display_digits(int now_hours, int now_mins, int closest_ring_hours, int closest_ring_minutes){
   /*Maybe some day it will become more beautiful*/
+  unsigned long local_timer = millis()
   lc.setDigit(0, 0, now_hours/10, false);
   lc.setDigit(0, 1, now_hours%10, false);
   lc.setDigit(0, 2, now_mins/10, false);
@@ -234,12 +144,10 @@ void display_digits(int now_hours, int now_mins, int closest_ring){
   lc.setDigit(0, 5, (closest_ring%1000)/100, false);
   lc.setDigit(0, 6, ((closest_ring%1000)%100)/10, false);
   lc.setDigit(0, 7, ((closest_ring%1000)%100)%10, false);
-  delay(500);
-  lc.clearDisplay(0);
 }
 void hard_reset(){
   scheduleReset();
-  for(int i = 0; i < 44; i++){
+  for(int i = 0; i < 256; i++){
     EEPROM.put(i, 0);
   }
 }
